ead8997e5df95909b8b4e8f7d18313ef637dd3af
eugene <paakwesinunoo135@gmail.com>
2023-04-09 17:29:06 +0100

    adding user routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify, request
 from models import db, User, Post
 
 app = Flask(__name__)
@@ -12,17 +12,21 @@ db.init_app(app)
 
 # Define your routes
 
-@app.route('/')
-def index():
-    users = User.query.all()
-    return render_template('index.html', users=users)
+@app.route('/users', methods=['GET'])
+def get_users():
+    Profile= User.query.all()
+    users = [user.to_dict() for user in Profile]
+    return jsonify(users)
 
-@app.route('/user/<int:user_id>')
-def user_profile(user_id):
-    user = User.query.get(user_id)
-    posts = user.posts
-    return render_template('user_profile.html', user=user, posts=posts)
-
+@app.route('/users/<int:id>', methods=['GET'])
+def get_vacation_spot(id):
+    spot = User.query.get(id)
+    if spot:
+        return jsonify(spot.to_dict())
+    else:
+        return jsonify({'error': 'User not found.'}), 404
 if __name__ == '__main__':
+    with app.app_context():  # Add this line to set up Flask application context
+        db.create_all()  # Move this line inside the app context
     app.run(debug=True)
 
--- models.py
+++ models.py
@@ -1,7 +1,14 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Configure the SQLAlchemy database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
 # Create an instance of SQLAlchemy
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
 # Define your database models
 
@@ -10,21 +17,24 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    profile = db.Column(db.String(255))
 
-    def __init__(self, name, email, password):
+    def __init__(self, name, email, password, profile):
         self.name = name
         self.email = email
         self.password = password
+        self.profile = profile
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('posts', lazy=True))
+    user = db.relationship('User')
 
     def __init__(self, title, content, user_id):
         self.title = title
         self.content = content
         self.user_id = user_id
+
 
