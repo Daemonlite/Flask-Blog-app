97143fb9e0d97b28655a39a16305a7c47482da4e
eugene <paakwesinunoo135@gmail.com>
2023-04-10 15:37:40 +0100

    adding time fields

--- models.py
+++ models.py
@@ -1,9 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+from datetime import datetime  # Import datetime for handling timestamps
 
+from datetime import datetime
 # Create an instance of SQLAlchemy
 db = SQLAlchemy()
 
-# Define your database models
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +16,8 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     profile = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __init__(self, name, email, password, profile):
         self.name = name
@@ -24,8 +31,12 @@ class User(db.Model):
             'name': self.name,
             'email': self.email,
             'password': self.password,
-            'profile':self.profile
+            'profile': self.profile,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
+
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,18 +44,25 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('posts', lazy=True))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __init__(self, title, content, user_id):
         self.title = title
         self.content = content
         self.user_id = user_id
+        self.created_at = datetime.utcnow()  # Set initial value for created_at
+        self.updated_at = datetime.utcnow()  # Set initial value for updated_at
 
-    
     def to_dict(self):
         return {
             'id': self.id,
             'title': self.title,
             'content': self.content,
             'user_id': self.user_id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
+
+
 
