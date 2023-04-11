5c7901281722ebebaf0bd804bdc4e08d31c43ab4
eugene <paakwesinunoo135@gmail.com>
2023-04-11 16:18:14 +0100

    homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import {useEffect,useState} from 'react'
+import axios from 'axios'
 
 const Home = () => {
+const [posts,setPost] = useState([])
+
+useEffect(()=>{
+axios.get('http://127.0.0.1:5000/posts')
+.then((res)=>console.log(res.data))
+.catch((err)=>console.log(err))
+
+},[posts])
+
   return (
-    <div>Home</div>
+    <div>
+
+    </div>
   )
 }
 
