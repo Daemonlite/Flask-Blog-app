7f3dd9bee4581194b45cfc029dca957645f35aa5
eugene <paakwesinunoo135@gmail.com>
2023-04-11 15:39:21 +0100

    loginrendered

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,21 +1,19 @@
-
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import jwt_decode from 'jwt-decode'
+import jwt_decode from "jwt-decode";
 const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
-  const [password, setpassword] = useState("");
-  const navigate = useNavigate()
-
+  const [password, setPassword] = useState("");
 
   const handleCallbackResponse = (response) => {
-    console.log("Encoded JWT ID Token : ",response.credential)
-    var userObject = jwt_decode(response.credential)
-    console.log(userObject)
-    const Password = `${userObject.given_name}wegvfejwy1@`
+    console.log("Encoded JWT ID Token : ", response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    const Password = `${userObject.given_name}wegvfejwy1@`;
     // Use the decoded JWT ID token to log the user in
     axios
       .post("http://localhost:5000/login/", {
@@ -28,40 +26,37 @@ const Login = () => {
           localStorage.setItem("userInfo", JSON.stringify(res.data));
           navigate("/");
         }
-        setpassword("");
-        setEmail("");
       })
       .catch((err) => {
-       toast.error("Login failed check credentials");
+        toast.error("Login failed check credentials");
       });
-  }
-  
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
-      client_id : '224073338639-us7klguo2oge970dmf953r2s79kvtt6n.apps.googleusercontent.com',
-      callback:handleCallbackResponse
-    })
+      client_id:
+        "224073338639-us7klguo2oge970dmf953r2s79kvtt6n.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
 
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      {theme:"outline",size:"large"}
-    )
+    if (document.getElementById("signInDiv")) {
+      google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+        theme: "outline",
+        size: "large",
+      });
+    }
+  }, []);
 
-    
-  },[])
-  google.accounts.id.prompt()
- 
   return (
-    <div className="Login">
-      <div className="signInDiv">
-        
-       
-
+    <div className="log">
+      <h2 className="head">Login to Continue</h2>
+      <div className="Login">
+        <div id="signInDiv"></div>
       </div>
     </div>
   );
 };
 
 export default Login;
+
